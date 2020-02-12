b99ef833e08a0a5bbe14549742c2d68505ada339
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-12 13:36:28 -0800

    logger is working

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const server = require('./server');
+
+
+
+server.use(function(req, res) {
+  res.status(404).send('Aint nobody got time for dat!!');
+});
+
+
+server.listen(5000, () => {
+  console.log('\n* Server Running on http://localhost:5000 *\n');
+});
