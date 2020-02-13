cb1422979d6726fac9abdeb4934a0366596cb3aa
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-13 09:07:07 -0800

    ready to deploy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // code away!
+require('dotenv').config(); // add this line as the first thing to run1
 const server = require('./server');
 
 
@@ -7,7 +8,10 @@ const server = require('./server');
 //   res.status(404).send('Aint nobody got time for dat!!');
 // });
 
+const port = process.env.PORT 
 
-server.listen(5000, () => {
+
+
+server.listen(port, () => {
   console.log('\n* Server Running on http://localhost:5000 *\n');
 });
